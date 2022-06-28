bbe3f34421e0a400760ed2ce9fb9eb2c570dbab7
Melki-244 <melkicavalcante244@gmail.com>
2022-06-28 15:00:39 -0300

    Instalando e Criando a Primeira Rota Com Gin | Primeira Rota Com Gin

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func ExibeTodosOsAlunos(c *gin.Context) {
+  c.JSON(200, gin.H{
+    "id":"1",
+    "nome":"Melki",
+  })
+}
 func main() {
   r := gin.Default()
+  r.GET("/alunos", ExibeTodosOsAlunos)
   r.Run()
 }
